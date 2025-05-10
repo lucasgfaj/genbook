764104dd1007c0cb57d3a1b57fef7f96ec2c22bb
Emanuel Oliveira Andrade <101219040+emanuel-o-dev@users.noreply.github.com>
2025-05-09 21:22:01 -0300

    @emanuelodev/2/feat/create login page (#7)
    * Add login method to HomeController
    
    * Add login view and fade-in animation to application layout
    
    * Update application.phtml

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -11,4 +11,9 @@ class HomeController extends Controller
         $title = 'Home Page';
         $this->render('home/index', compact('title'));
     }
+    public function login(): void
+    {
+        $title = 'Login';
+        $this->render('login/login', compact('title'));
+    }
 }
