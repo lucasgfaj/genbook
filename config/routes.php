6bef71ced50d878844a7a83797ccf652ef310208
Emanuel Oliveira Andrade <101219040+emanuel-o-dev@users.noreply.github.com>
2025-05-13 22:20:50 -0300

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -4,7 +4,6 @@ use App\Controllers\HomeController;
 use App\Controllers\AuthController;
 use Core\Router\Route;
 
-
 Route::get('/', [AuthController::class, 'index'])->name('users.login');
 Route::post('/login', [AuthController::class, 'authenticate'])->name('users.authenticate');
 
