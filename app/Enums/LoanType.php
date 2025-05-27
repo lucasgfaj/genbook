6a1682e55d48be091f5a41ef5099189e758cd32b
emanuel-o-dev <emanueloliveiraandrade@alunos.utfpr.edu.br>
2025-05-27 16:13:03 -0300

    feat: Add LoanType enum and update Loan model to use it

---
+++ app/Enums/LoanType.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums;
+
+enum LoanType: int
+{
+    case Book = 1;
+    case Material = 2;
+}
--- app/Models/Loan.php
+++ app/Models/Loan.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\LoanType;
 use Lib\Validations;
 use Core\Database\ActiveRecord\BelongsTo;
 use Core\Database\ActiveRecord\Model;
@@ -11,7 +12,7 @@ use Core\Database\ActiveRecord\Model;
  * @property int $user_id
  * @property int $staff_id
  * @property int $type_id
- * @property int $enum_type
+ * @property LoanType $enum_type
  * @property string $loan_date
  * @property string $due_date
  * @property string $return_date
@@ -19,6 +20,7 @@ use Core\Database\ActiveRecord\Model;
  * @property string $created_at
  * @property string $updated_at
  */
+
 class Loan extends Model
 {
     protected static string $table = 'loans';
@@ -45,17 +47,8 @@ class Loan extends Model
         return $this->belongsTo(Staff::class, 'staff_id');
     }
 
-    /**
-     * Returns the related Book or Material based on the enum_type.
-     *
-     * @return BelongsTo|null
-     */
-    public function type(): ?BelongsTo
+    public function type(): BelongsTo
     {
-        return match ((int) $this->enum_type) {
-            1 => $this->belongsTo(Book::class, 'type_id'),
-            2 => $this->belongsTo(Material::class, 'type_id'),
-            default => null,
-        };
+        return $this->belongsTo(LoanType::class, 'enum_type');
     }
 }
