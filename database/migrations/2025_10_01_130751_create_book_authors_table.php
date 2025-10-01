e5f37f52648d5d8f131f5c8ac875722383c36b82
lucasgfaj <lucasgfajardo09@gmail.com>
2025-10-01 10:45:00 -0300

    Add: Migrations Database

---
+++ database/migrations/2025_10_01_130751_create_book_authors_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('book_authors', function (Blueprint $table) {
+            $table->foreignId('book_id')->constrained()->onDelete('cascade');
+            $table->foreignId('author_id')->constrained()->onDelete('cascade');
+            $table->primary(['book_id', 'author_id']);
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('book_authors');
+    }
+};
