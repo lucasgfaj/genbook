cac11e7482f232bd381e902e1d36d3342f5f91c2
lucasgfaj <lucasgfajardo09@gmail.com>
2025-10-04 18:47:51 -0300

    Add: favicon

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
+
+// Initial Page
 Route::get('/', function () {
     return view('public.auth');
 })->name('auth');
