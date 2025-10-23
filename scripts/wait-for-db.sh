070b8b0c8a373575d43575394a3fae9719edb0f9
lucasgfaj <lucasgfaj09@gmail.com>
2025-10-22 23:19:34 -0300

    edit: ci

--- scripts/wait-for-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Cria o banco de teste se não existir
-psql -h db -U "$DB_USERNAME" -tc "SELECT 1 FROM pg_database WHERE datname = 'genbook_test'" | grep -q 1 || \
-psql -h db -U "$DB_USERNAME" -c "CREATE DATABASE genbook_test"
-
-echo "✅ Banco genbook_test garantido!"
